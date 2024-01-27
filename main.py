4fdece82c8318923bad9a606e9cb32bd0ce52b0e
Salloumgh <salloumghanem99@Gmail.com>
2024-01-27 19:13:20 +0200

    added requirements file and dundder fucntino

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+import uvicorn
 from routes.api import router 
 
 app=FastAPI()
 app.include_router(router)
 
-app.get('/')
-def test():
-    return "test"
+
+if __name__ == '__main__':
+    uvicorn.run(
+        "__main__:app", reload=True, workers = 2
+    )
